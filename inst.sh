5f715c4c176228a74e19bae4922442b5c9435b04
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-08-17 12:05:03 -0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -30,6 +30,9 @@ su - kiosk -c "echo | cp autostart.sh /home/kiosk/.config/openbox"
 su - kiosk -c "echo | chmod +x /home/kiosk/.config/openbox/autostart.sh"
 chown kiosk:kiosk /usr/share/wallpapers/default.jpg
 # VNC Server
+cp x11vnc.service /etc/systemd/system/
+systemctl restart daemon-reload
+systemctl enable --now x11vnc
 echo "Enter the VNC remote access password"
 x11vnc -storepasswd
 #Cleaning up
